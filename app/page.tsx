b060318a068cf1405b08230e4678d8ac05d9b1b1
sumanthmx <70922037+sumanthmx@users.noreply.github.com>
2023-07-11 15:15:00 -0400

    7/11/2023

--- app/page.tsx
+++ app/page.tsx
@@ -22,81 +22,89 @@ export default function Home() {
      'Authentic Patient Testimonials and Reviews', 'Round-the-Clock Customer Support',
      'Post-Treatment Care Information']
   return (
-    <div className = "flex flex-col min-h-screen relative">
-        <div className = "bg-white h-92 py-16 flex flex-col sm:flex-row">
-            <div className="w-full sm:w-2/5 font-helvetica-neue sm:ml-10 sm:mr-0 text-center sm:text-left">
-                <span className="text-blue-400 text-xl sm:text-2xl font-bold">Your Gateway <span className="text-blue-950 text-xl sm:text-2xl font-bold">To World Class Health Care</span></span>
-                <p className="mt-6 text-blue-950 text-md">Quality Care Global is a rapidly expanding network consisting of the screened,
-                    accredited doctors, clinics and hospitals in the world. Find the perfect healthcare providers to suit your budget
-                    and medical needs, obtain treatment abroad with confidence.</p>
-                <div className = "hidden sm:block text-left mt-8">
-                    <a href="/" className="text-sm font-helvetica-neue bg-blue-950 rounded text-white px-24 py-3">
-                      Get Started
+    <div className = "flex flex-col min-h-screen relative mx-auto">
+        <div className = "bg-white">
+            <div className = "py-10 sm:py-16 flex flex-col sm:flex-row mx-auto container relative items-center">
+                <div className="w-full sm:w-1/2 font-helvetica-neue sm:ml-10 px-4 sm:mr-0 text-center sm:text-left z-10">
+                    <span className="text-blue-400 text-2xl sm:text-3xl font-bold">Your Gateway <span className="text-blue-950 text-2xl sm:text-3xl font-bold">To World Class Health Care</span></span>
+                    <p className="mt-6 text-blue-950 text-md">Quality Care Global is a rapidly expanding network consisting of the screened,
+                        accredited doctors, clinics and hospitals in the world. Find the perfect healthcare providers to suit your budget
+                        and medical needs, obtain treatment abroad with confidence.</p>
+                    <div className = "hidden sm:block text-left mt-8">
+                        <a href="/" className="text-sm font-helvetica-neue bg-blue-950 rounded text-white px-24 py-3">
+                          Get Started
+                        </a>
+                    </div>
+                </div>
+                <img src="/landing_img.png" alt="Home Globe Pic" className={`${styles.map_img} right-2 absolute hidden sm:block sm:w-3/5 mr-4 ml-2 object-cover z-0`}/>
+            </div>
+        </div>
+        <div className = "bg-blue-950">
+            <div className = "h-92 py-16 mx-auto container">
+                <p className="sm:ml-6 mx-4 text-white text-2xl font-helvetica-neue
+                font-bold text-center sm:text-left"> Medical Care Abroad Simplified</p>
+                <div className="flex flex-col grid grid-cols-1 md:grid-cols-2 gap-4 my-6 mx-10 text-white">
+                    <div className="relative flex">
+                        <img src="home_icons/hp1.png" alt="Home Icon 1" className="w-12 h-12"/>
+                        <div>
+                            <p className="ml-4 mt-2 font-helvetica-neue font-bold text-lg">1.  Explore Providers</p>
+                            <p className="flex align-text-top ml-4 mt-3 font-helvetica-neue text-md">Browse our global network of accredited healthcare
+                                professionals and hospitals.</p>
+                        </div>
+                    </div>
+                    <div className="relative flex">
+                        <img src="home_icons/hp2.png" alt="Home Icon 2" className="w-12 h-12"/>
+                        <div>
+                          <p className="ml-4 mt-2 font-helvetica-neue font-bold text-lg">2.  Request Consultation</p>
+                            <p className="flex align-text-top ml-4 mt-3 font-helvetica-neue text-md">A specialist from our team will collaborate with you
+                                to comprehend your medical requirements, and personal preferences.</p>
+                        </div>
+                    </div>
+                    <div className="relative flex">
+                        <img src="home_icons/hp3.png" alt="Home Icon 3" className="w-12 h-12"/>
+                        <div>
+                            <p className="ml-4 mt-2 font-helvetica-neue font-bold text-lg">3.  Plan Travel</p>
+                            <p className="flex align-text-top ml-4 mt-3 font-helvetica-neue text-md">Arrange your travel itinerary independently or with our assistance.</p>
+                        </div>
+                    </div>
+                    <div className="relative flex">
+                        <img src="home_icons/hp4.png" alt="Home Icon 4" className="w-12 h-12"/>
+                        <div>
+                            <p className="ml-4 mt-2 font-helvetica-neue font-bold text-lg">4.  Experience Care</p>
+                            <p className="flex align-text-top ml-4 mt-3 font-helvetica-neue text-md">Enjoy world-class, personalized healthcare from your chosen provider.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className = "text-center mt-10">
+                    <a href="/" className="text-sm font-helvetica-neue bg-white rounded text-blue-950 px-12 py-3">
+                      Search Specialist
                     </a>
                 </div>
             </div>
-            <img src="/globe.png" alt="Home Globe Pic" className="w-16 h-72 hidden sm:block sm:w-3/5 mr-4 ml-2 object-cover"/>
         </div>
-        <div className = "bg-blue-950 h-92 py-16">
-            <p className="sm:ml-6 mx-4 text-white text-2xl font-helvetica-neue
-            font-bold text-center sm:text-left"> Medical Care Abroad Simplified</p>
-            <div className="flex flex-col grid grid-cols-1 md:grid-cols-2 gap-4 my-6 mx-10 text-white">
-                <div className="relative flex">
-                    <img src="home_icons/hp1.png" alt="Home Icon 1" className="w-12 h-12"/>
-                    <div>
-                        <p className="ml-4 mt-2 font-helvetica-neue font-bold text-lg">1.  Explore Providers</p>
-                        <p className="flex align-text-top ml-4 mt-3 font-helvetica-neue text-md">Browse our global network of accredited healthcare
-                            professionals and hospitals.</p>
-                    </div>
-                </div>
-                <div className="relative flex">
-                    <img src="home_icons/hp2.png" alt="Home Icon 2" className="w-12 h-12"/>
-                    <div>
-                      <p className="ml-4 mt-2 font-helvetica-neue font-bold text-lg">2.  Request Consultation</p>
-                        <p className="flex align-text-top ml-4 mt-3 font-helvetica-neue text-md">A specialist from our team will collaborate with you
-                            to comprehend your medical requirements, and personal preferences.</p>
-                    </div>
-                </div>
-                <div className="relative flex">
-                    <img src="home_icons/hp3.png" alt="Home Icon 3" className="w-12 h-12"/>
-                    <div>
-                        <p className="ml-4 mt-2 font-helvetica-neue font-bold text-lg">3.  Plan Travel</p>
-                        <p className="flex align-text-top ml-4 mt-3 font-helvetica-neue text-md">Arrange your travel itinerary independently or with our assistance.</p>
-                    </div>
-                </div>
-                <div className="relative flex">
-                    <img src="home_icons/hp4.png" alt="Home Icon 4" className="w-12 h-12"/>
-                    <div>
-                        <p className="ml-4 mt-2 font-helvetica-neue font-bold text-lg">4.  Experience Care</p>
-                        <p className="flex align-text-top ml-4 mt-3 font-helvetica-neue text-md">Enjoy world-class, personalized healthcare from your chosen provider.</p>
-                    </div>
-                </div>
-            </div>
-            <div className = "text-center mt-10">
-                <a href="/" className="text-sm font-helvetica-neue bg-white rounded text-blue-950 px-12 py-3">
-                  Search Specialist
-                </a>
+        <div className = "bg-white">
+            <div className = "mx-auto container py-8">
+                <p className="sm:ml-6 mx-4 mt-2 text-blue-950 text-2xl font-helvetica-neue font-bold text-center sm:text-left"> Our Commitment to Trustworthiness</p>
+                <p className="sm:ml-6 mx-4 mt-3 mb-4 text-blue-950 text-md text-center sm:text-left font-helvetica-neue "> Discover the steps we're taking to ensure
+                    you feel secure when traveling abroad:</p>
+                <Carousel images={imgFolder} bodies={paras} names={names}/>
             </div>
         </div>
-        <div className = "bg-white py-8">
-            <p className="sm:ml-6 mx-4 mt-2 text-blue-950 text-2xl font-helvetica-neue font-bold text-center sm:text-left"> Our Commitment to Trustworthiness</p>
-            <p className="sm:ml-6 mx-4 mt-3 mb-4 text-blue-950 text-md text-center sm:text-left font-helvetica-neue "> Discover the steps we're taking to ensure
-                you feel secure when traveling abroad:</p>
-            <Carousel images={imgFolder} bodies={paras} names={names}/>
-        </div>
-        <div className = "bg-slate-200 h-54 font-helvetica-neue py-8">
-            <p className="text-center text-blue-950 font-bold text-2xl mt-2">Stay Updated</p>
-            <p className="text-center text-blue-950 text-md ml-16 mr-16 mt-4">Stay inspired and informed
-                with the newest health and wellness updates, delivered to your inbox daily.</p>
-            <div className="flex items-center justify-center mt-6">
-                <input type="text" placeholder="Type your email address"
-                       className="text-center border border-gray-300 rounded-l py-2 px-6
-                       focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-                <button
-                    className="text-center bg-blue-950 text-white rounded-r py-0 sm:py-2
-                    px-6 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">
-                    Sign Up
-                </button>
+        <div className = "bg-slate-200">
+            <div className = "h-54 font-helvetica-neue py-8 mx-auto container">
+                <p className="text-center text-blue-950 font-bold text-2xl mt-2">Stay Updated</p>
+                <p className="text-center text-blue-950 text-md ml-16 mr-16 mt-4">Stay inspired and informed
+                    with the newest health and wellness updates, delivered to your inbox daily.</p>
+                <div className="flex items-center justify-center mt-6">
+                    <input type="text" placeholder="Type your email address"
+                           className="border border-gray-300 rounded-l h-12 px-6
+                           focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+                    <button
+                        className="text-center bg-blue-950 text-white rounded-r
+                        px-4 h-12 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                        Sign Up
+                    </button>
+                </div>
             </div>
         </div>
     </div>
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -6,64 +6,66 @@ import {useState} from 'react';
 const Navbar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   return (
-    <nav className="bg-white py-4">
-      <div className="container mx-auto px-4 flex justify-between items-center">
-        <a href = "/">
-          <Image src={"/QCG_Logo_White Background.png"} alt="QCG Logo" className="h-10" height={70} width={100}/>
-        </a>
-        {/* Dropdown Menu */}
-        <div className="md:hidden relative">
-          <button
-            className="text-blue-800 hover:text-blue-900 focus:outline-none"
-            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-          >
-            <div className="flex flex-col justify-center items-center w-8 h-8">
-              <span className="bg-gray-900 h-1 w-full mb-1"></span>
-              <span className="bg-gray-900 h-1 w-full mb-1"></span>
-              <span className="bg-gray-900 h-1 w-full"></span>
-            </div>
-          </button>
-          {isDropdownOpen && (
-            <ul className="absolute top-0 right-0 mt-10 py-2 bg-white border rounded z-10">
-              <li>
-                <Link href="/support" className="block px-4 py-2 text-sm text-gray-700 hover:text-blue-800">Find a specialist</Link>
-              </li>
-              <li>
-                <Link href="/how" className="block px-4 py-2 text-sm text-gray-700 hover:text-blue-800">How it works</Link>
-              </li>
-              <li>
-                <Link href="/about" className="block px-4 py-2 text-sm text-gray-700 hover:text-blue-800">About</Link>
-              </li>
-              <li>
-                <Link href="/blog" className="block px-4 py-2 text-sm text-gray-700 hover:text-blue-800">Blog</Link>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:text-blue-800"
-                >
-                  List your clinic
-                </a>
-              </li>
-            </ul>
-          )}
-        </div>
-
-        {/* Regular Navbar */}
-        <ul className="hidden md:flex space-x-4 ml-auto">
-          <li className="inline-block">
-            <Link href="/support" className="text-sm font-helvetica-neue text-gray-700 hover:text-blue-800 mr-6 w-32">Find a specialist</Link>
-            <Link href="/how" className="text-sm font-helvetica-neue text-gray-700 hover:text-blue-800 mr-6 w-32">How it works</Link>
-            <Link href="/about" className="text-sm font-helvetica-neue text-gray-700 hover:text-blue-800 mr-6 w-32">About</Link>
-            <Link href="/blog" className="text-sm font-helvetica-neue text-gray-700 hover:text-blue-800 mr-14 w-32">Blog</Link>
-            <a
-              href="/"
-              className="text-sm font-helvetica-neue text-gray-300 bg-blue-950 px-4 py-2 rounded text-white w-32"
+    <nav className="bg-white">
+      <div className = "py-4 container mx-auto">
+        <div className="container mx-auto px-4 flex justify-between items-center">
+          <a href = "/">
+            <Image src={"/QCG_Logo_White Background.png"} alt="QCG Logo" className="h-10" height={70} width={100}/>
+          </a>
+          {/* Dropdown Menu */}
+          <div className="md:hidden relative">
+            <button
+              className="text-blue-800 hover:text-blue-900 focus:outline-none"
+              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
             >
-              List your clinic
-            </a>
-          </li>
-        </ul>
+              <div className="flex flex-col justify-center items-center w-8 h-8">
+                <span className="bg-gray-900 h-1 w-full mb-1"></span>
+                <span className="bg-gray-900 h-1 w-full mb-1"></span>
+                <span className="bg-gray-900 h-1 w-full"></span>
+              </div>
+            </button>
+            {isDropdownOpen && (
+              <ul className="absolute top-0 right-0 mt-10 py-2 bg-white border rounded z-10">
+                <li>
+                  <Link href="/support" className="block px-4 py-2 text-sm text-gray-700 hover:text-blue-800">Find a specialist</Link>
+                </li>
+                <li>
+                  <Link href="/how" className="block px-4 py-2 text-sm text-gray-700 hover:text-blue-800">How it works</Link>
+                </li>
+                <li>
+                  <Link href="/about" className="block px-4 py-2 text-sm text-gray-700 hover:text-blue-800">About</Link>
+                </li>
+                <li>
+                  <Link href="/blog" className="block px-4 py-2 text-sm text-gray-700 hover:text-blue-800">Blog</Link>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className="block px-4 py-2 text-sm text-gray-700 hover:text-blue-800"
+                  >
+                    List your clinic
+                  </a>
+                </li>
+              </ul>
+            )}
+          </div>
+
+          {/* Regular Navbar */}
+          <ul className="hidden md:flex space-x-4 ml-auto">
+            <li className="inline-block">
+              <Link href="/support" className="text-sm font-helvetica-neue text-gray-700 hover:text-blue-800 mr-6 w-32">Find a specialist</Link>
+              <Link href="/how" className="text-sm font-helvetica-neue text-gray-700 hover:text-blue-800 mr-6 w-32">How it works</Link>
+              <Link href="/about" className="text-sm font-helvetica-neue text-gray-700 hover:text-blue-800 mr-6 w-32">About</Link>
+              <Link href="/blog" className="text-sm font-helvetica-neue text-gray-700 hover:text-blue-800 mr-14 w-32">Blog</Link>
+              <a
+                href="/"
+                className="text-sm font-helvetica-neue text-gray-300 bg-blue-950 px-4 py-2 rounded text-white w-32"
+              >
+                List your clinic
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
